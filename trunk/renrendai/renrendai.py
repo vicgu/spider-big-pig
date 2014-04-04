23542f0bcbdec365acdc2c9e2b549cfbd0684e69
vicgu123@gmail.com <vicgu123@gmail.com@d2e0f966-fc94-17eb-ae72-0db32d0cdd56>
2014-04-04 07:58:04 +0000

    personInfo borrowRecords bidRecord ---by vicgu

---
+++ trunk/renrendai/renrendai.py
@@ -0,0 +1,271 @@
+# coding=utf-8
+# FileName: renrendai.py
+# Description:
+#           Spider
+# Author:   Gu, Jianyu
+# Create Date:
+#           Apr 1, 2014
+# Mail:     vicgu123@gmail.com
+#
+
+from renrendai_config import *
+import sys, urllib2, cookielib, json, time, re
+sys.path.append("../")
+import db
+
+# Login procedure
+# Return: login opener
+def login():
+  # Create cookieJar & cookie handler
+  cookieJar = cookielib.CookieJar()
+  opener    = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
+  
+  opener.addheaders = LOGIN_HAD
+  opener.open(LOGIN_URL, LOGIN_DTA)
+  
+  if isLogin(cookieJar) == False:
+    print "-------> login failed, check your name & password in 'renrendai_config.py' file"
+    exit(0)
+  
+  return opener
+  
+def isLogin(cookieJar):
+  for cookie in cookieJar:
+    if cookie.name == "jforumUserInfo":
+      return True
+  return False
+  
+# Get loan id list
+# Return: Get Loan List ids
+def getLoanlist(opener):
+  loanIds = []
+
+  # travel with loanList,  Max trying times
+  for pageId in range(LOAN_LIST_BEG, LOAN_LIST_END + 1):
+    # Get json
+    time.sleep(VISIT_TIME)
+    url = LOAN_LIST_URL + str(pageId)
+    isSuccess = False
+    try:
+      response  = opener.open(url)
+      isSuccess = True
+    except:
+      for i in range(MAX_RETRY_TIME):
+        try:
+          time.sleep(RETRY_TIME)
+          response  = opener.open(url)
+          isSuccess = True
+          break
+        except:
+          continue
+          
+    if isSuccess == False:
+      continue
+      
+    # Json parser
+    try:
+      try:
+        raw_page = response.read().decode('utf-8')
+      except:
+        print "-------> get id try to use gbk", pageId
+        raw_page = response.read().decode('gbk')
+            
+      jsonDict = json.loads(raw_page)
+      
+      for loan in jsonDict['data']['loans']:
+        loanIds.append(loan[LOAN_ID_WORD])
+        print "----> get", loan[LOAN_ID_WORD]
+    except Exception, data:
+      print "---->[Exception]", data
+      print "---->[Error] Json parser error for loanPage %d" % pageId
+      continue
+
+  # return id list
+  return loanIds
+
+def saveUserInfoToDB(db, infoUser, infoItems, bidRecords):
+  try:
+    personID = db.getPersonID(infoUser[0], DB_SOURCE)
+    # check whether personInfo exists. if not, save it
+    if personID < 0:
+      columns = "nickName, updateTime, age, gender, infoSource, url"
+      values = "'%s', '%s', '%s', '%s', '%s', '%s'" % (infoUser[0], str(time.time()), infoUser[4], infoUser[1], DB_SOURCE, DB_URL)
+      sql = "insert into personInfo (%s) values (%s);" % (columns, values)
+      personID = db.insert(sql)
+
+    # save borrowRecords to DB
+    columns = "personID, updateTime, moneyAmount, interestRate, duration, payment, infoSource, url, monthPay"
+    values = "'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'" % (personID, str(time.time()), infoItems[0], infoItems[1], infoItems[2], infoItems[5], DB_SOURCE, DB_URL, infoItems[6])
+    sql = "insert into borrowRecords (%s) values (%s);" % (columns, values)
+    borrowID = db.insert(sql)
+
+    # save bidRecords to DB
+    columns = "borrowID, personID, bidMoney, updateTime, url, bidTime"
+    for loan in bidRecords:
+      bidPersonName = loan['userNickName']
+      bidPersonID = db.getPersonID(bidPersonName, DB_SOURCE)
+      if bidPersonID < 0:
+        sql = "insert into personInfo (nickName, updateTime, infoSource, url) values ('%s', '%s', '%s', '%s');" % (bidPersonName, str(time.time()), DB_SOURCE, DB_URL)
+        bidPersonID = db.insert(sql)
+      values = "'%s', '%s', '%s', '%s', '%s', '%s'" % (borrowID, bidPersonID, str(loan['amount']), str(time.time()), DB_URL, loan['lendTime'])
+      sql = "insert into bidRecord (%s) values (%s);" % (columns, values)
+      bidID = db.insert(sql)
+
+  except Exception, data:
+    print "Exception:", data
+
+def getPriInfoById(loanId, opener):
+  url = PRIVATE_URL + str(loanId)
+
+  isFail = False
+  try:
+    response = opener.open(url)
+  except:
+    for i in range(MAX_RETRY_TIME):
+      try:
+        print "-------> retry", loanId
+        time.sleep(RETRY_TIME)
+        response = opener.open(url)
+        isFail = False
+        break
+      except:
+        isFail = True
+        continue
+
+  if isFail == True:
+    return ""
+  
+  print "----------> private get successfully", loanId
+  
+  database = db.DB()
+  database.open()
+
+  try:
+    pageString = response.read().decode('utf-8')
+  except:
+    print "-------> try to use gbk to parser loan id page"
+    pageString = response.read().decode('gbk')    
+  print "----------> private page parser successfully", loanId
+
+  # print string to a file for debugging
+  #tmpFile = open(str(loanId) + ".txt", "w+")
+  #tmpFile.write(pageString.encode('utf-8'))
+  #tmpFile.close()
+
+  # check if READY or REPAYING
+  if pageString.find("class=\"READY\"") >= 0:
+    uniregexp = RE_READY.decode('utf-8')
+  elif pageString.find("class=\"REPAYING\"") >= 0:
+    uniregexp = RE_REPAYING.decode('utf-8')
+  elif pageString.find("class=\"OPEN\"") >= 0:
+    print "----------> Loan is not completed."
+    uniregexp = RE_READY.decode('utf-8')
+  else:
+    print "Error! Loan state is unrecognized."
+    exit(1)
+  
+#  uniregexp = REG_EXP.decode('utf-8')
+  # get userInfo & loanInfo
+  infoUser = re.findall(RE_USER.decode('utf-8'), pageString, re.S)
+  infoItems = re.findall(uniregexp, pageString, re.S)
+  print "----------> private page regular parser successfully", loanId
+
+  infoString = ""
+  for i in range(len(infoUser[0])):
+    infoString += infoUser[0][i].encode('gbk') + ','
+  infoString = infoString[:-1] + '\n'
+  for i in range(len(infoItems[0])):
+    infoString += infoItems[0][i].encode('gbk') + ','
+  infoString = infoString[:-1] + '\n'
+
+  # get bid records
+  url = BID_RECORD_URL + str(loanId)
+  isSuccess = False
+  try:
+    response = opener.open(url)
+    isSuccess = True
+  except:
+    for i in range(MAX_RETRY_TIME):
+      try:
+        time.sleep(RETRY_TIME)
+        response = opener.open(url)
+        isSuccess = True
+        break
+      except:
+        continue
+  if isSuccess == False:
+    print "----------> Cannot get bid records from ID " + str(loanId)
+  try:
+    try:
+      raw_page = response.read().decode('utf-8')
+    except:
+      raw_page = response.read().decode('gbk')
+    jsonDict = json.loads(raw_page)
+    for loan in jsonDict['data']['lenderRecords']:
+      infoString += loan['userNickName'].encode('gbk') + ','
+      infoString += str(loan['amount']).encode('gbk') + ','
+      infoString += loan['lendTime'].encode('gbk') + '\n'
+  except Exception, data:
+    print "---------->[Exception]", data
+    print "---------->[Error] Json parser error for loanId %d" % loanId
+#  for item in infoItems:
+#    infoString = item[0].encode('gbk')  + ',' + \
+#                 item[1].encode('gbk')  + "," + \
+#                 item[2].encode('gbk')  + "," + \
+#                 item[3].encode('gbk')  + "," + \
+#                 item[4].encode('gbk')  + "," + \
+#                 item[5].encode('gbk')  + ',' + \
+#                 item[6].encode('gbk') + '\n'
+  
+  saveUserInfoToDB(database, infoUser[0], infoItems[0], jsonDict['data']['lenderRecords'])
+
+  database.close()
+
+  return infoString
+  
+def getPriInfo(outputFile, opener, loanIds):
+  # Write output file title
+  outputFile.write(RE_TITLES.decode('utf-8').encode('gbk'))
+  
+  # Loop with loan id list getting private info
+  i = 0
+  while i < len(loanIds):
+    time.sleep(VISIT_TIME)
+    print "-------> try to get loan id", loanIds[i]
+    priInfo = getPriInfoById(loanIds[i], opener)    
+    print "-------> success to get loan id", loanIds[i]
+    
+    if len(priInfo) > 0:
+      outputFile.write(priInfo)
+      i += 1
+      print priInfo
+    else:
+      print "Fresh too quick, retry after", RETRY_TIME
+      time.sleep(RETRY_TIME)
+
+# Spider entrance
+def renrendaiSpider():
+  # Login
+  print "----> login"
+  opener = login()
+  print "----> login successfully\n"
+  
+  # Get loan list
+  print "----> getting loan ids"
+  loanIds = getLoanlist(opener)
+  print "----> loan ids getting successfully\n"
+  
+  # Get loan list
+  print "----> getting private info"
+  outputFile = open(OUTPUT_FILE, "w+")
+  getPriInfo(outputFile, opener, loanIds)
+  outputFile.close()
+  print "----> getting successfully\n"  
+ 
+def main():
+  if __name__ == "__main__":
+    print "===== renrendai spider Start ====="
+    renrendaiSpider()
+    print "===== renrendai spider End ====="
+    
+main()
---
+++ trunk/renrendai/renrendai_config.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+# FileName: renrendai_config.py
+# Description:
+#           Configuration variables for renrendai.com
+# Author:   Gu, Jianyu
+# Create Date:
+#           Apr 1, 2014
+# Mail:     vicgu123@gmail.com
+#
+
+LOGIN_URL     = "https://www.renrendai.com/j_spring_security_check"
+LOGIN_USR     = "18801970616" # Set your login name here
+LOGIN_PSD     = "rrdkey110" # Set your password here
+LOGIN_DTA     = "j_username=%s&j_password=%s" % (LOGIN_USR, LOGIN_PSD)
+LOGIN_HAD     = [("Accept", "text/html, application/xhtml+xml, */*"), \
+                 ("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)"), \
+                 ("Content-Type", "application/x-www-form-urlencoded"), \
+                 ("Host", "www.renrendai.com")]
+                 
+LOAN_LIST_URL = "http://www.renrendai.com/lend/loanList!json.action?pageIndex="
+LOAN_LIST_BEG = 1
+LOAN_LIST_END = 1
+LOAN_ID_WORD  = "loanId"
+
+DB_SOURCE     = "renrendai"
+DB_URL        = "www.renrendai.com"
+
+PRIVATE_URL   = "http://www.renrendai.com/lend/detailPage.action?loanId="
+
+BID_RECORD_URL= "http://www.renrendai.com/lend/getborrowerandlenderinfo.action?id=lenderRecords&loanId="
+
+MAX_RETRY_TIME= 3
+RETRY_TIME    = 60
+VISIT_TIME    = 3
+
+OUTPUT_FILE   = "renrendai.csv"
+
+RE_USER       = '用户名.*?<em.*?title=\"(.*?)\".*?title=\"(.*?)\".*?公司行业.*?<span.*?>(.*?)<.*?收入范围.*?<span.*?>(.*?)<.*?年&nbsp;&nbsp;&nbsp;&nbsp;龄.*?<span.*?>(.*?)<.*?公司规模.*?<span.*?>(.*?)<.*?学&nbsp;&nbsp;&nbsp;&nbsp;历.*?<span.*?>(.*?)<.*?岗位职位.*?<span.*?>(.*?)<.*?学&nbsp;&nbsp;&nbsp;&nbsp;校.*?title=\"(.*?)\".*?工作城市.*?<span.*?>(.*?)<.*?婚&nbsp;&nbsp;&nbsp;&nbsp;姻.*?<span.*?>(.*?)<.*?工作时间.*?<span.*?>(.*?)<'
+RE_READY      = '标的总额.*?<em.*?>(.*?)<.*?年利率.*?<em.*?>(.*?)<.*?还款期限.*?<em.*?>(.*?)<.*?保障方式.*?<span.*?>(.*?)<.*?提前还款费率.*?<em.*?>(.*?)<.*?还款方式.*?<span.*?>(.*?)<.*?月还本息.*?<em.*?>(.*?)<'
+RE_REPAYING   = '标的总额.*?<em.*?>(.*?)<.*?年利率.*?<em.*?>(.*?)<.*?还款期限.*?<em.*?>(.*?)<.*?保障方式.*?<span.*?>(.*?)<.*?提前还款费率.*?<em.*?>(.*?)<.*?还款方式.*?<span.*?>(.*?)<.*?月还本息.*?<em.*?>(.*?)<.*?待还本息.*?<span.*?>(.*?)<.*?剩余期数.*?<span.*?>(.*?)<.*?下一合约还款日.*?<span.*?>(.*?)<'
+REG_EXP       = '用户档案.*?ID\s*?(\d*?)</span>.*?nickname-text.*?>(.*?)<.*?ui-icon-gender.*?"(.)".*?公司行业</span>.*?>(.*?)</span>.*?年龄</span>.*?>(\d*?)</span>.*?公司规模</span>.*?>(.*?)</span>.*?学历</span>.*?>(.*?)</span>.*?工作城市</span>.*?>(.*?)</span>.*?学校</span>.*?>(.*?)</span>.*?工作时间</span>.*?>(.*?)</span>.*?婚姻</span>.*?>(.*?)</span>.*?岗位职位</span>.*?>(.*?)</span>.*?收入范围</span>.*?>(.*?)</span>'
+
+RE_TITLES     = ''
+REG_TITLES    = '用户ID, 用户名, 性别, 公司行业, 年龄, 公司规模, 学历, 工作城市, 学校, 工作时间, 婚姻, 职位, 收入范围\n'
