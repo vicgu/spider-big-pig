bf734c5a18003fff9aef31073dad03ea3ac3ae6f
vicgu123 <vicgu123@gmail.com>
2014-05-10 13:21:14 +0000

    ppdai by Gu Jianyu

--- ppdai/ppdai.py
+++ ppdai/ppdai.py
@@ -10,7 +10,7 @@
 #           xiaoning.liu.leon@gmail.com
 
 from ppdai_config import *
-import sys, urllib2, urllib, cookielib, json, time, re
+import sys, urllib2, urllib, cookielib, time, re
 sys.path.append("../")
 import log, db
 
@@ -60,8 +60,44 @@ def getLoanInfo(loanId, opener):
     pageString = response.read().decode('gbk')
   #print pageString
   infoLoan = re.findall(RE_LOAN.decode('utf-8'), pageString, re.S)
+  infoBorrow = re.findall(RE_BORROW.decode('utf-8'), pageString, re.S)
+  infoLoanList = []
   for i in range(len(infoLoan[0])):
-    print ''.join(''.join(infoLoan[0][i].split()).split(','))
+    infoLoanList.append(''.join(''.join(infoLoan[0][i].split()).split(',')))
+
+  database = db.DB()
+  database.open()
+
+  saveLoanInfoToDB(database, infoLoanList, infoBorrow)
+
+def saveLoanInfoToDB(db, infoLoan, infoBorrow):
+  try:
+    # check whether personInfo exists. if not, save it
+    personID = db.getPersonID(infoLoan[4], INFO_SOURCE)
+    if personID < 0:
+      sql = "insert into personInfo (nickName, infoSource, updateTime) values ('%s', '%s', '%s');" % (infoLoan[4], INFO_SOURCE, str(time.time()))
+      personID = db.insert(sql)
+
+    # save borrowRecords to DB
+    columns = "personID, updateTime, moneyAmount, interestRate, duration, infoSource, url, monthPay"
+    values = "'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'" % (personID, str(time.time()), infoLoan[0], infoLoan[1], infoLoan[2], INFO_SOURCE, INFO_URL, infoLoan[3])
+    sql = "insert into borrowRecords (%s) values (%s);" % (columns, values)
+    borrowID = db.insert(sql)
+
+    # save bidRecords to DB
+    columns = "borrowID, personID, bidMoney, updateTime, url"
+    for borrow in infoBorrow:
+      bidPersonName = ''.join(''.join(borrow[0].split()).split(','))
+      bidMoney = ''.join(''.join(borrow[2].split()).split(','))
+      bidPersonID = db.getPersonID(bidPersonName, INFO_SOURCE)
+      if bidPersonID < 0:
+        sql = "insert into personInfo (nickName, infoSource, updateTime) values ('%s', '%s', '%s');" % (bidPersonName, INFO_SOURCE, str(time.time()))
+        bidPersonID = db.insert(sql)
+      values = "'%s', '%s', '%s', '%s', '%s'" % (borrowID, bidPersonID, bidMoney, str(time.time()), INFO_URL)
+      sql = "insert into bidRecord (%s) values (%s);" % (columns, values)
+      bidID = db.insert(sql)
+  except Exception, data:
+    print "[Exception]", data
 
 def main():
   if __name__ == "__main__":
